ff4e8c100e411def19d70428f805374d071ef570
joaojv90 <joao.jaramillo@gmail.com>
2022-12-06 10:29:57 -0500

    test

--- src/test/java/ec/edu/itsqmet/evaluation/EvaluationApplicationTests.java
+++ src/test/java/ec/edu/itsqmet/evaluation/EvaluationApplicationTests.java
@@ -1,13 +1,25 @@
 package ec.edu.itsqmet.evaluation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ec.edu.itsqmet.evaluation.repository.IBirdRepository;
+import ec.edu.itsqmet.evaluation.singleton.BirdSingleton;
 
 @SpringBootTest
 class EvaluationApplicationTests {
+	
+	@Autowired
+	private IBirdRepository birdRepository
 
 	@Test
 	void contextLoads() {
+		
+		for(int i=0; i <= 5; i++) {
+			System.out.println(BirdSingleton.getTotalBird(birdRepository));
+		}
+		
 	}
 
 }
