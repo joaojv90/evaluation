8315cff519055ded7ee2266613075a9c4ffb42e4
joaojv90 <joao.jaramillo@gmail.com>
2022-12-06 09:39:00 -0500

    first commit

---
+++ src/test/java/ec/edu/itsqmet/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
